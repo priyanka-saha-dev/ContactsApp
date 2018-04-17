099e10d96926221ca7184de369072531d098f697
Pri-ddc-laptop <priyanka.njoy9@gmail.com>
2018-04-18 00:22:06 +0530

    - Adding changes

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -1,16 +1,32 @@
-var contactsApp = angular.module('contactsApp', []); 
+var contactsApp = angular.module('contactsApp', []);
 
-contactsApp.controller('contactsCtrl', ['$scope', '$http', function($scope, $http) { 
-    console.log("Hello World from controller"); 
+contactsApp.controller('contactsCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log("Hello World from controller");
 
-    $http.get('/contactList').then(successCallback, errorCallback);
+    var refresh = function () {
+        $http.get('/contactList').
+            then(function (response) {
+                console.log("Refresh success : " + response.status);
+                $scope.contactList = response.data;
+                $scope.message = 'Bingo!!';
+            }, function (error) {
+                console.log("Refresh failure : " + error);
+                $scope.message = 'Problem occured while refresh..';
+            });
+    };
 
-    function successCallback(response){
-        $scope.contactList = response.data;
-        $scope.message = 'Bingo!!'
-    }
-    function errorCallback(error){
-        $scope.message = 'Problem occured..';
-    } 
-    
+    refresh();
+
+    $scope.addContact = function () {
+        $http.post('/contactList', $scope.contact).then(function (response) {
+            console.log("Add contact success : " + response.status);
+            $scope.contact = {};
+            $scope.message = 'Bingo!!';
+            refresh();
+        }, function(error) {
+            console.log("Add contact failure : " + error);
+            $scope.message = 'Problem occured while saving..';
+        });
+    };
+
 }]);
--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 var express = require('express');
 var app  = express();
 
+var bodyparser = require('body-parser');
+
+var mongojs = require('mongojs');
+var db  = mongojs('contactList',['contactList']);
+
 app.use(express.static(__dirname+"/public"));
+app.use(bodyparser.json());
 
 app.get('/contactList', function(req, res){
 
     console.log("Getting all contact list from server");
 
-    person1 = {
-        name : 'Priyanka Saha',
-        number : '7278952122',
-        email : 'ps@mail.com',
-    };
+    db.contactList.find(function (err, docx){
+        //if(!err) {
+            console.log(docx);
+            res.status(200).json(docx);
+        //}
 
-    person2 = {
-        name : 'Tuhin Raha',
-        number : '8013584734',
-        email : 'tr@mail.com',
-    };
+    });
+});
 
-    person3 = {
-        name : 'Mitra Saha',
-        number : '9830117785',
-        email : 'ms@mail.com',
-    };
-
-    var contactList = [person1, person2, person3];
-
-    res.json(contactList);
-
+app.post('/contactList', function(req, res){
+    console.log(req.body);
+    db.contactList.insert(req.body, function(err, docx) {
+        res.status(200).json(docx);
+    });
+    
 });
 
 app.listen(8080);
