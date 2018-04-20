21ae229223ec4a0d0f253bf409ae96cae1e83f14
DPSA129 <priyanka.saha.external@axa.be>
2018-04-20 15:51:02 +0530

    - completed

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -1,7 +1,7 @@
 var contactsApp = angular.module('contactsApp', []);
 
 contactsApp.controller('contactsCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("Hello World from controller");
+    console.log("Contacts App controller");
 
     var refresh = function () {
         $http.get('/contactList').
@@ -9,6 +9,7 @@ contactsApp.controller('contactsCtrl', ['$scope', '$http', function ($scope, $ht
                 console.log("Refresh success : " + response.status);
                 $scope.contactList = response.data;
                 $scope.message = 'Bingo!!';
+                $scope.enableUpdate = false;
             }, function (error) {
                 console.log("Refresh failure : " + error);
                 $scope.message = 'Problem occured while refresh..';
@@ -18,15 +19,65 @@ contactsApp.controller('contactsCtrl', ['$scope', '$http', function ($scope, $ht
     refresh();
 
     $scope.addContact = function () {
-        $http.post('/contactList', $scope.contact).then(function (response) {
-            console.log("Add contact success : " + response.status);
-            $scope.contact = {};
-            $scope.message = 'Bingo!!';
-            refresh();
-        }, function(error) {
-            console.log("Add contact failure : " + error);
-            $scope.message = 'Problem occured while saving..';
-        });
+
+        if ($scope.contact && $scope.contact.name && $scope.contact.email && $scope.contact.number) {
+            $http.post('/contactList', $scope.contact)
+                .then(function (response) {
+                    console.log("Add contact success : " + response.status);
+                    $scope.contact = {};
+                    $scope.message = 'Bingo!!';
+                    refresh();
+                }, function (error) {
+                    console.log("Add contact failure : " + error);
+                    $scope.message = 'Problem occured while saving..';
+                });
+        }
+
+    };
+
+    $scope.deleteContact = function (id) {
+
+        console.log("Delete for : " + id);
+        $http.delete('/contactList/' + id)
+            .then(function (response) {
+                console.log("Delete contact success : " + response.status);
+                $scope.contact = {};
+                $scope.message = 'Bingo!!';
+                refresh();
+            }, function (error) {
+                console.log("Add contact failure : " + error);
+                $scope.message = 'Problem occured while deleting..';
+            });
+    };
+
+    $scope.editContact = function (id) {
+
+        console.log("Edit for : " + id);
+        $http.get('/contactList/' + id)
+            .then(function (response) {
+                console.log("Get contact - " + id + " success : " + response.status);
+                $scope.contact = response.data;
+                $scope.message = 'Bingo!!';
+                $scope.enableUpdate = true;
+            }, function (error) {
+                console.log("Get contact - " + id + " failure : " + error);
+                $scope.message = 'Problem occured while fetching contact#' + id;
+            });
+    };
+
+    $scope.updateContact = function () {
+
+        console.log("Updating contact for : " + $scope.contact._id);
+        $http.put('/contactList/' + $scope.contact._id, $scope.contact)
+            .then(function (response) {
+                console.log("Update contact - " + $scope.contact._id + " success : " + response.status);
+                $scope.contact = {};
+                $scope.message = 'Bingo!!';
+                refresh();
+            }, function (error) {
+                console.log("Update contact - " + $scope.contact._id + " failure : " + error);
+                $scope.message = 'Problem occured while fetching contact#' + $scope.contact._id;
+            });
     };
 
 }]);
--- server.js
+++ server.js
@@ -1,33 +1,65 @@
 var express = require('express');
-var app  = express();
+var app = express();
 
 var bodyparser = require('body-parser');
 
 var mongojs = require('mongojs');
-var db  = mongojs('contactList',['contactList']);
+var db = mongojs('contactList', ['contactList']);
 
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyparser.json());
 
-app.get('/contactList', function(req, res){
-
+app.get('/contactList', function (req, res) {
     console.log("Getting all contact list from server");
-
-    db.contactList.find(function (err, docx){
-        //if(!err) {
-            console.log(docx);
-            res.status(200).json(docx);
-        //}
-
+    db.contactList.find(function (err, docx) {
+        console.log(docx);
+        res.status(200).json(docx);
     });
 });
 
-app.post('/contactList', function(req, res){
-    console.log(req.body);
-    db.contactList.insert(req.body, function(err, docx) {
+app.get('/contactList/:id', function (req, res) {
+    
+    var id = req.params.id;
+    console.log("Getting contact list from server for ID : " + id);
+    
+    db.contactList.findOne({ _id: mongojs.ObjectId(id) }, function (err, docx) {
+        console.log(docx);
         res.status(200).json(docx);
     });
-    
+});
+
+app.post('/contactList', function (req, res) {
+    console.log(req.body);
+    db.contactList.insert(req.body, function (err, docx) {
+        res.status(200).json(docx);
+    });
+
+});
+
+app.delete('/contactList/:id', function (req, res) {
+    var id = req.params.id;
+    console.log("deleting for ID : " + id);
+    db.contactList.remove({ _id: mongojs.ObjectId(id) }, function (err, docx) {
+        res.status(200).json(docx);
+    });
+
+});
+
+app.put('/contactList/:id', function (req, res) {
+    var id = req.params.id;
+    console.log("updating for ID : " + id);
+
+    console.log("updating for name : " + req.body.name);
+    console.log("updating for email : " + req.body.email);
+    console.log("updating for number : " + req.body.number);
+    var queryBody = {
+        query: { _id: mongojs.ObjectId(id) },
+        update: { $set: { name: req.body.name, email: req.body.email, number: req.body.number } },
+        new: true
+    }
+    db.contactList.findAndModify(queryBody, function (err, docx) {
+            res.status(200).json(docx);
+    });
 });
 
 app.listen(8080);
